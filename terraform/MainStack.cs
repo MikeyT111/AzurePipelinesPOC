7bedb95260688420e439d6e29f90a0f614af2040
Mike Taylor <MikeTaylor@Mikes-MacBook-Pro.local>
2023-08-18 22:35:31 +0100

    feat: updated to have some items in the terraform file. But it is failing to build

--- terraform/MainStack.cs
+++ terraform/MainStack.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Constructs;
 using HashiCorp.Cdktf;
+using HashiCorp.Cdktf.Providers.Azurerm;
 
 
 namespace MyCompany.MyApp
@@ -9,7 +11,25 @@ namespace MyCompany.MyApp
     {
         public MainStack(Construct scope, string id) : base(scope, id)
         {
-            // define resources here
+             new AzurermProvider(this, "AzureRm", new AzurermProviderConfig {
+                Features = new AzurermProviderFeatures()
+            });
+
+            new VirtualNetwork(this, "TfVnet", new VirtualNetworkConfig {
+                Location = "uksouth",
+                AddressSpace = new [] {"10.0.0.0/24"},
+                Name = "TerraformVNet",
+                ResourceGroupName = "<YOUR_RESOURCE_GROUP_NAME>"
+            }); new AzurermProvider(this, "AzureRm", new AzurermProviderConfig {
+                Features = new AzurermProviderFeatures()
+            });
+
+            new VirtualNetwork(this, "TfVnet", new VirtualNetworkConfig {
+                Location = "uksouth",
+                AddressSpace = new [] {"10.0.0.0/24"},
+                Name = "TerraformVNet",
+                ResourceGroupName = "<YOUR_RESOURCE_GROUP_NAME>"
+            });
         }
     }
 }
