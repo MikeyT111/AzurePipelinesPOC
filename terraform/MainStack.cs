b3c3c07c35cf254dd4f4ea6379aa1ab827739b8f
Mike Taylor <MikeTaylor@Mikes-MacBook-Pro.local>
2023-08-20 15:20:06 +0100

    feat: Updated the terraform stack so that it actually deploys it an AWS Account.

--- terraform/MainStack.cs
+++ terraform/MainStack.cs
@@ -14,23 +14,19 @@ namespace MyCompany.MyApp
         public MainStack(Construct scope, string id) : base(scope, id)
         {
              new AzurermProvider(this, "AzureRm", new AzurermProviderConfig {
-                Features = new AzurermProviderFeatures()
+                Features = new AzurermProviderFeatures(),
+                SubscriptionId = "",
+                ClientId = "",
+                ClientSecret = "",
+                TenantId = "",
+                SkipProviderRegistration = true
             });
 
             new VirtualNetwork(this, "TfVnet", new VirtualNetworkConfig {
                 Location = "uksouth",
                 AddressSpace = new [] {"10.0.0.0/24"},
                 Name = "TerraformVNet",
-                ResourceGroupName = "<YOUR_RESOURCE_GROUP_NAME>"
-            }); new AzurermProvider(this, "AzureRm", new AzurermProviderConfig {
-                Features = new AzurermProviderFeatures()
-            });
-
-            new VirtualNetwork(this, "TfVnet", new VirtualNetworkConfig {
-                Location = "uksouth",
-                AddressSpace = new [] {"10.0.0.0/24"},
-                Name = "TerraformVNet",
-                ResourceGroupName = "<YOUR_RESOURCE_GROUP_NAME>"
+                ResourceGroupName = "MikesApp"
             });
         }
     }
